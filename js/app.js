7995520254ea163d5f1a54bb16230977032e7f71
Lauren Main <lauren.main28@gmail.com>
2022-05-04 19:34:30 -0700

    next 4 questions

--- js/app.js
+++ js/app.js
@@ -18,3 +18,61 @@ function outbound () {
  }
 }
   outbound();
+
+  function hikingFool () {
+
+    let answer2 = prompt ("Have I visited and hiked in all 3 National Parks in the state of Washington?") .toLowerCase();
+  
+    if (answer2 === 'yes' || answer2 === 'y') {
+      alert ("Yes, I backpacked in Olympic National Park last summer.");
+    } else if (answer2 ==='no'|| answer2 ==='n') {
+      alert ("But they are so close. I couldn't help but visit.");
+    } else {
+      alert ("Please answer with a yes or no");
+   }
+  }
+    hikingFool();
+
+    function snacks () {
+
+      let answer3 = prompt ("Have I visited all the National Parks in Utah?") .toLowerCase();
+    
+      if (answer3 === 'yes' || answer3 === 'y') {
+        alert ("Yes! Capitol Reef has orchards that are known for their pies!");
+      } else if (answer3 ==='no'|| answer3 ==='n') {
+        alert ("But Capitol Reef features pies made in the National Park!");
+      } else {
+        alert ("Please answer with a yes or no");
+     }
+    }
+      snacks();
+      
+
+      function trailMix () {
+
+        let answer4 = prompt ("Have I visited all the National Parks in California?") .toLowerCase();
+      
+        if (answer4 === 'yes' || answer4 === 'y') {
+          alert ("Yes! Death Valley is one of the coolest places I have ever been.");
+        } else if (answer4 ==='no'|| answer4 ==='n') {
+          alert ("Oh no! California is a great road trip.");
+        } else {
+          alert ("Please answer with a yes or no");
+       }
+      }
+        trailMix();  
+
+
+        function compassRose () {
+
+          let answer5 = prompt ("Have I visited the Grand Canyon?") .toLowerCase();
+        
+          if (answer5 === 'yes' || answer5 === 'y') {
+            alert ("Yes, I watched a big snow storm roll into the canyon.");
+          } else if (answer5 ==='no'|| answer5 ==='n') {
+            alert ("I had to see the Grand Canyon!");
+          } else {
+            alert ("Please answer with a yes or no");
+         }
+        }
+          compassRose();  
