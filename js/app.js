f48ea6c0d2543356424c3a213a4d54ba81a642e9
Lauren Main <lauren.main28@gmail.com>
2022-05-04 19:51:28 -0700

    add console.log messages, add outro prompt

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ function outbound () {
   let answer1 = prompt ("Before I visit a National Park do I check out their website and pack the 10 essentials?") .toLowerCase();
 
   if (answer1 === 'yes' || answer1 === 'y') {
+    //console.log ("Yes, it pays to be prepared.");
     alert ("Yes, it pays to be prepared.");
   } else if (answer1 ==='no'|| answer1 ==='n') {
+    //console.log ("Oh no! It pays to research!");
     alert ("Oh no! It pays to research!");
   } else {
     alert ("Please answer with a yes or no");
+    //console.log ("Please answer with a yes or no");
  }
 }
   outbound();
@@ -24,10 +27,13 @@ function outbound () {
     let answer2 = prompt ("Have I visited and hiked in all 3 National Parks in the state of Washington?") .toLowerCase();
   
     if (answer2 === 'yes' || answer2 === 'y') {
+      //console.log ("Yes, I backpacked in Olympic National Park last summer.");
       alert ("Yes, I backpacked in Olympic National Park last summer.");
     } else if (answer2 ==='no'|| answer2 ==='n') {
+      //console.log ("But they are so close. I couldn't help but visit.");
       alert ("But they are so close. I couldn't help but visit.");
     } else {
+      //console.log ("Please answer with a yes or no");
       alert ("Please answer with a yes or no");
    }
   }
@@ -38,10 +44,13 @@ function outbound () {
       let answer3 = prompt ("Have I visited all the National Parks in Utah?") .toLowerCase();
     
       if (answer3 === 'yes' || answer3 === 'y') {
+        //console.log ("Yes! Capitol Reef has orchards that are known for their pies!");
         alert ("Yes! Capitol Reef has orchards that are known for their pies!");
       } else if (answer3 ==='no'|| answer3 ==='n') {
+        //console.log ("But Capitol Reef features pies made in the National Park!");
         alert ("But Capitol Reef features pies made in the National Park!");
       } else {
+        //console.log ("Please answer with a yes or no");
         alert ("Please answer with a yes or no");
      }
     }
@@ -53,10 +62,13 @@ function outbound () {
         let answer4 = prompt ("Have I visited all the National Parks in California?") .toLowerCase();
       
         if (answer4 === 'yes' || answer4 === 'y') {
+          //console.log ("Yes! Death Valley is one of the coolest places I have ever been.");
           alert ("Yes! Death Valley is one of the coolest places I have ever been.");
         } else if (answer4 ==='no'|| answer4 ==='n') {
+          //console.log ("Oh no! California is a great road trip.");
           alert ("Oh no! California is a great road trip.");
         } else {
+          //console.log ("Please answer with a yes or no");
           alert ("Please answer with a yes or no");
        }
       }
@@ -68,11 +80,16 @@ function outbound () {
           let answer5 = prompt ("Have I visited the Grand Canyon?") .toLowerCase();
         
           if (answer5 === 'yes' || answer5 === 'y') {
+            //console.log ("Yes, I watched a big snow storm roll into the canyon.");
             alert ("Yes, I watched a big snow storm roll into the canyon.");
           } else if (answer5 ==='no'|| answer5 ==='n') {
+            //console.log ("I had to see the Grand Canyon!");
             alert ("I had to see the Grand Canyon!");
           } else {
+            //console.log ("Please answer with a yes or no");
             alert ("Please answer with a yes or no");
          }
         }
           compassRose();  
+
+          let outro = prompt ("Thanks so much " + user +"! Welcome to my page!");
