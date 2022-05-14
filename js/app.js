ebec75adfdd5a16823426544ce04991daa3bacd4
Lauren Main <lauren.main28@gmail.com>
2022-05-14 15:42:32 -0700

    function corrections and run prettier, delete console logs

--- js/app.js
+++ js/app.js
@@ -1,197 +1,153 @@
 "use strict";
 
-// function fiveQuestions(answer) {
-//   answer = answer.toLowerCase();
-//   if (answer === "yes") {
-//     return true;
-//   } else if (answer === "no") {
-//     return false;
-//   }
-// }
-// let answer1 = prompt('Before I visit a National Park do I check out their website and pack the 10 essentials?');
-// console.log(fiveQuestions(answer1));
-// let answer2 = prompt('Have I visited and hiked in all 3 National Parks in the state of Washington?');
-// console.log(fiveQuestions(answer2));
-// let answer3 = prompt('Have I visited all the National Parks in Utah?');
-// console.log(fiveQuestions(answer3));
-// let answer4 = prompt('Have I visited all the National Parks in California?');
-// console.log(fiveQuestions(answer4));
-// let answer5 = prompt('Have I visited the Grand Canyon?');
-// console.log(fiveQuestions(answer5));
+function fiveQuestions(answer) {
+  answer = answer.toLowerCase();
+  if (answer === "yes" || answer === "y") {
+    alert("Correct!");
+  } else if (answer === "no" || answer === "n") {
+    alert("Sorry...incorrect");
+  }
+}
 
-// let playerScore = 0;
-// let user = prompt("Hello, what is your name?");
+let answer1 = prompt(
+  "Before I visit a National Park do I check out their website and pack the 10 essentials?"
+);
+let answer2 = prompt(
+  "Have I visited and hiked in all 3 National Parks in the state of Washington?"
+);
+let answer3 = prompt("Have I visited all the National Parks in Utah?");
+let answer4 = prompt("Have I visited all the National Parks in California?");
+let answer5 = prompt("Have I visited the Grand Canyon?");
 
-// let intro = alert(
-//   "Welcome to my page " +
-//     user +
-//     "! Welcome to a guessing game about my goal to visit all the US National Parks! Please answer yes or no to the next 5 questions."
-// );
-// //prompt might do better for the game as an alert after testing user experience
+fiveQuestions(answer1);
+fiveQuestions(answer2);
+fiveQuestions(answer3);
+fiveQuestions(answer4);
+fiveQuestions(answer5);
 
-//   //revision, combine everyting into one function
-//   let answer1 = prompt(
-//     "Before I visit a National Park do I check out their website and pack the 10 essentials?"
-//   )?.toLowerCase();
+let playerScore = 0;
+let user = prompt("Hello, what is your name?");
 
-//   if (answer1 === "yes" || answer1 === "y") {
-//     //console.log ("Yes, it pays to be prepared.");
-//     alert("Yes, it pays to be prepared.");
-//     playerScore++;
-//   } else if (answer1 === "no" || answer1 === "n") {
-//     //console.log ("Oh no! It pays to research!");
-//     alert("Oh no! It pays to research!");
-//   } else {
-//     alert("Please answer with a (y)es or (n)o");
-//     //console.log ("Please answer with a yes or no");
-//   }
+let intro = alert(
+  "Welcome to my page " +
+    user +
+    "! Welcome to a guessing game about my goal to visit all the US National Parks! Please answer yes or no to the next 5 questions."
+);
 
-//   let answer2 = prompt(
-//     "Have I visited and hiked in all 3 National Parks in the state of Washington?"
-//   )?.toLowerCase();
+let answer1a = prompt(
+  "Before I visit a National Park do I check out their website and pack the 10 essentials?"
+)?.toLowerCase();
 
-//   if (answer2 === "yes" || answer2 === "y") {
-//     //console.log ("Yes, I backpacked in Olympic National Park last summer.");
-//     alert("Yes, I backpacked in Olympic National Park last summer.");
-//     playerScore++;
-//   } else if (answer2 === "no" || answer2 === "n") {
-//     //console.log ("But they are so close. I couldn't help but visit.");
-//     alert("But they are so close. I couldn't help but visit.");
-//   } else {
-//     //console.log ("Please answer with a yes or no");
-//     alert("Please answer with a (y)es or (n)o");
-//   }
+if (answer1a === "yes" || answer1a === "y") {
+  alert("Yes, it pays to be prepared.");
+  playerScore++;
+} else if (answer1a === "no" || answer1a === "n") {
+  alert("Oh no! It pays to research!");
+} else {
+  alert("Please answer with a (y)es or (n)o");
+}
 
-//   let answer3 = prompt(
-//     "Have I visited all the National Parks in Utah?"
-//   )?.toLowerCase();
+let answer2a = prompt(
+  "Have I visited and hiked in all 3 National Parks in the state of Washington?"
+)?.toLowerCase();
 
-//   if (answer3 === "yes" || answer3 === "y") {
-//     //console.log ("Yes! Capitol Reef has orchards that are known for their pies!");
-//     alert("Yes! Capitol Reef has orchards that are known for their pies!");
-//     playerScore++;
-//   } else if (answer3 === "no" || answer3 === "n") {
-//     //console.log ("But Capitol Reef features pies made in the National Park!");
-//     alert("But Capitol Reef features pies made in the National Park!");
-//   } else {
-//     //console.log ("Please answer with a yes or no");
-//     alert("Please answer with a (y)es or (n)o");
-//   }
+if (answer2a === "yes" || answer2a === "y") {
+  alert("Yes, I backpacked in Olympic National Park last summer.");
+  playerScore++;
+} else if (answer2a === "no" || answer2a === "n") {
+  alert("But they are so close. I couldn't help but visit.");
+} else {
+  alert("Please answer with a (y)es or (n)o");
+}
 
-//   let answer4 = prompt(
-//     "Have I visited all the National Parks in California?"
-//   )?.toLowerCase();
+let answer3a = prompt(
+  "Have I visited all the National Parks in Utah?"
+)?.toLowerCase();
 
-//   if (answer4 === "yes" || answer4 === "y") {
-//     //console.log ("Yes! Death Valley is one of the coolest places I have ever been.");
-//     alert("Yes! Death Valley is one of the coolest places I have ever been.");
-//     playerScore++;
-//   } else if (answer4 === "no" || answer4 === "n") {
-//     //console.log ("Oh no! California is a great road trip.");
-//     alert("Oh no! California is a great road trip.");
-//   } else {
-//     //console.log ("Please answer with a yes or no");
-//     alert("Please answer with a (y)es or (n)o");
-//   }
+if (answer3a === "yes" || answer3a === "y") {
+  alert("Yes! Capitol Reef has orchards that are known for their pies!");
+  playerScore++;
+} else if (answer3a === "no" || answer3a === "n") {
+  alert("But Capitol Reef features pies made in the National Park!");
+} else {
+  alert("Please answer with a (y)es or (n)o");
+}
 
-//   let answer5 = prompt("Have I visited the Grand Canyon?")?.toLowerCase();
+let answer4a = prompt(
+  "Have I visited all the National Parks in California?"
+)?.toLowerCase();
 
-//   if (answer5 === "yes" || answer5 === "y") {
-//     //console.log ("Yes, I watched a big snow storm roll into the canyon.");
-//     alert("Yes, I watched a big snow storm roll into the canyon.");
-//     playerScore++;
-//   } else if (answer5 === "no" || answer5 === "n") {
-//     //console.log ("I had to see the Grand Canyon!");
-//     alert("I had to see the Grand Canyon!");
-//   } else {
-//     //console.log ("Please answer with a yes or no");
-//     alert("Please answer with a (y)es or (n)o");
-//   }
+if (answer4a === "yes" || answer4a === "y") {
+  alert("Yes! Death Valley is one of the coolest places I have ever been.");
+  playerScore++;
+} else if (answer4a === "no" || answer4a === "n") {
+  alert("Oh no! California is a great road trip.");
+} else {
+  alert("Please answer with a (y)es or (n)o");
+}
 
-// //let outro = prompt("Thanks so much " + user + "! Welcome to my page!");
-// //comment out outro due to new questions
+let answer5a = prompt("Have I visited the Grand Canyon?")?.toLowerCase();
 
-// let correctAnswer = 27;
-// let guesses = 4;
+if (answer5a === "yes" || answer5a === "y") {
+  alert("Yes, I watched a big snow storm roll into the canyon.");
+  playerScore++;
+} else if (answer5a === "no" || answer5a === "n") {
+  alert("I had to see the Grand Canyon!");
+} else {
+  alert("Please answer with a (y)es or (n)o");
+}
 
-// while (guesses) {
-//   let natGuess = prompt(
-//     "Can you guess how many National Parks I have visited? Hint: there are only 63 National Parks."
-//   );
+let correctAnswer = 27;
+let guesses = 4;
 
-//   guesses--;
-//   if (natGuess > correctAnswer) {
-//     alert("That number is too high.");
-//   } else if (natGuess < correctAnswer) {
-//     alert("That number is too low.");
-//   } else if (natGuess == 27) {
-//     alert("Correct! Congratulations!");
-//     playerScore++;
-//     break;
-//   }
-//   if (guesses === 0) {
-//     alert("Only 4 guesses today! The number was " + correctAnswer);
-//   }
-// }
+while (guesses) {
+  let natGuess = prompt(
+    "Can you guess how many National Parks I have visited? Hint: there are only 63 National Parks."
+  );
 
-// let favParks = [
-//   "Yellowstone",
-//   "Death Valley",
-//   "Canyonlands",
-//   "Redwoods",
-//   "Mount Lassen",
-// ];
-// let correctPark = false;
-// let parksGuess = 6;
+  guesses--;
+  if (natGuess > correctAnswer) {
+    alert("That number is too high.");
+  } else if (natGuess < correctAnswer) {
+    alert("That number is too low.");
+  } else if (natGuess == 27) {
+    alert("Correct! Congratulations!");
+    playerScore++;
+    break;
+  }
+  if (guesses === 0) {
+    alert("Only 4 guesses today! The number was " + correctAnswer);
+  }
+}
 
-// while (parksGuess && !correctPark) {
-
-//   let favParksQuestions = prompt(
-//     "What is one of my top favorite National Parks? You have " + parksGuess + " guesses.")?.toLowerCase();
-
-//   for (let i = 0; i < favParks.length; i++) {
-//     let favPark = favParks[i]?.toLowerCase();
-//     //console.log (i,favParks);
-//     if (favParksQuestions === favPark) {
-//       alert("Excellent guess! " + favParks + " are all of the correct answers!");
-//       correctPark = true;
-//       playerScore++;
-//     }
-
-//   }
-//   parksGuess--;
-//   // if (parksGuess !== 0) {
-//   //   alert("Try another one. You have " + parksGuess + " more guesses.");
-//   // } else {
-//   //   alert(favParks + " are all of the correct answers!");
-//   // }
-// }
-
-// alert("Thanks for playing " + user + ". You scored " + playerScore + "!");
-
-let parksAnswers = ["yes", "no",];
-
-let parksQuestions = [
-  "Before I visit a National Park do I check out their website and pack the 10 essentials?",
-  "Have I visited and hiked in all 3 National Parks in the state of Washington?",
-  "Have I visited all the National Parks in Utah?",
-  "Have I visited all the National Parks in California?",
-  "Have I visited the Grand Canyon?",
+let favParks = [
+  "Yellowstone",
+  "Death Valley",
+  "Canyonlands",
+  "Redwoods",
+  "Mount Lassen",
 ];
+let correctPark = false;
+let parksGuess = 6;
 
-let parksAsk;
+while (parksGuess && !correctPark) {
+  let favParksQuestions = prompt(
+    "What is one of my top favorite National Parks? You have " +
+      parksGuess +
+      " guesses."
+  )?.toLowerCase();
 
-function fiveQs() {
-   parksAsk = prompt(parksQuestions[i]);
-  for (let i = 0; i < parksQuestions.length; i++) {
-    //let parksQuestion = parksQuestions[i].toLowerCase();
-    if (parksAsk.toLowerCase()=== parksAnswers[i]){
-      alert ("Excellent guess!");
-    } 
-   
-   // if (parksQuestions === "yes") {
-      //console.log ("Excellent guess!");
+  for (let i = 0; i < favParks.length; i++) {
+    let favPark = favParks[i]?.toLowerCase();
+    if (favParksQuestions === favPark) {
+      alert(
+        "Excellent guess! " + favParks + " are all of the correct answers!"
+      );
+      correctPark = true;
+      playerScore++;
     }
   }
+  parksGuess--;
+}
 
-//2 arrays= question, correct answers
+alert("Thanks for playing " + user + ". You scored " + playerScore + "!");
