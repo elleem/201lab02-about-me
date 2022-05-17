ec3f937f55547cca1a1a8b98542b56efd04ee320
Lauren Main <lauren.main28@gmail.com>
2022-05-16 21:35:37 -0700

    function round up

--- js/app.js
+++ js/app.js
@@ -1,108 +1,99 @@
-"use strict";
-
-function fiveQuestions(answer) {
-  answer = answer.toLowerCase();
-  if (answer === "yes" || answer === "y") {
-    alert("Correct!");
-  } else if (answer === "no" || answer === "n") {
-    alert("Sorry...incorrect");
-  }
-}
-
-let answer1 = prompt(
-  "Before I visit a National Park do I check out their website and pack the 10 essentials?"
-);
-let answer2 = prompt(
-  "Have I visited and hiked in all 3 National Parks in the state of Washington?"
-);
-let answer3 = prompt("Have I visited all the National Parks in Utah?");
-let answer4 = prompt("Have I visited all the National Parks in California?");
-let answer5 = prompt("Have I visited the Grand Canyon?");
-
-fiveQuestions(answer1);
-fiveQuestions(answer2);
-fiveQuestions(answer3);
-fiveQuestions(answer4);
-fiveQuestions(answer5);
-
+// "use strict";
 let playerScore = 0;
 let user = prompt("Hello, what is your name?");
 
 let intro = alert(
   "Welcome to my page " +
     user +
-    "! Welcome to a guessing game about my goal to visit all the US National Parks! Please answer yes or no to the next 5 questions."
+    "! Welcome to a guessing game about my goal to visit all the US National Parks! Please answer yes or no to the next 5 questions to play for a score."
 );
 
-let answer1a = prompt(
+function hikingFool() {
+let answer1 = prompt(
   "Before I visit a National Park do I check out their website and pack the 10 essentials?"
 )?.toLowerCase();
 
-if (answer1a === "yes" || answer1a === "y") {
+if (answer1 === "yes" || answer1 === "y") {
   alert("Yes, it pays to be prepared.");
   playerScore++;
-} else if (answer1a === "no" || answer1a === "n") {
+} else if (answer1 === "no" || answer1 === "n") {
   alert("Oh no! It pays to research!");
 } else {
   alert("Please answer with a (y)es or (n)o");
 }
-
-let answer2a = prompt(
-  "Have I visited and hiked in all 3 National Parks in the state of Washington?"
-)?.toLowerCase();
-
-if (answer2a === "yes" || answer2a === "y") {
-  alert("Yes, I backpacked in Olympic National Park last summer.");
-  playerScore++;
-} else if (answer2a === "no" || answer2a === "n") {
-  alert("But they are so close. I couldn't help but visit.");
-} else {
-  alert("Please answer with a (y)es or (n)o");
 }
 
-let answer3a = prompt(
+function compassRose() {
+  let answer2 = prompt(
+    "Have I visited and hiked in all 3 National Parks in the state of Washington?"
+    )?.toLowerCase();
+    
+    if (answer2 === "yes" || answer2 === "y") {
+      alert("Yes, I backpacked in Olympic National Park last summer.");
+      playerScore++;
+    } else if (answer2 === "no" || answer2 === "n") {
+      alert("But they are so close. I couldn't help but visit.");
+    } else {
+      alert("Please answer with a (y)es or (n)o");
+    }
+  }
+ 
+ 
+
+function tenEssentials(){
+  let answer3 = prompt(
   "Have I visited all the National Parks in Utah?"
 )?.toLowerCase();
 
-if (answer3a === "yes" || answer3a === "y") {
+if (answer3 === "yes" || answer3 === "y") {
   alert("Yes! Capitol Reef has orchards that are known for their pies!");
   playerScore++;
-} else if (answer3a === "no" || answer3a === "n") {
+} else if (answer3 === "no" || answer3 === "n") {
   alert("But Capitol Reef features pies made in the National Park!");
 } else {
   alert("Please answer with a (y)es or (n)o");
 }
+}
 
-let answer4a = prompt(
+function leaveNoTrace(){
+let answer4 = prompt(
   "Have I visited all the National Parks in California?"
 )?.toLowerCase();
 
-if (answer4a === "yes" || answer4a === "y") {
+if (answer4 === "yes" || answer4 === "y") {
   alert("Yes! Death Valley is one of the coolest places I have ever been.");
   playerScore++;
-} else if (answer4a === "no" || answer4a === "n") {
+} else if (answer4 === "no" || answer4 === "n") {
   alert("Oh no! California is a great road trip.");
 } else {
   alert("Please answer with a (y)es or (n)o");
 }
+}
 
-let answer5a = prompt("Have I visited the Grand Canyon?")?.toLowerCase();
+function walkWoods(){
+let answer5 = prompt("Have I visited the Grand Canyon?")?.toLowerCase();
 
-if (answer5a === "yes" || answer5a === "y") {
+if (answer5 === "yes" || answer5 === "y") {
   alert("Yes, I watched a big snow storm roll into the canyon.");
   playerScore++;
-} else if (answer5a === "no" || answer5a === "n") {
+} else if (answer5 === "no" || answer5 === "n") {
   alert("I had to see the Grand Canyon!");
 } else {
   alert("Please answer with a (y)es or (n)o");
 }
+}
+hikingFool();
+compassRose();
+tenEssentials();
+leaveNoTrace();
+walkWoods();
 
 let correctAnswer = 27;
 let guesses = 4;
 
 while (guesses) {
   let natGuess = prompt(
-    "Can you guess how many National Parks I have visited? Hint: there are only 63 National Parks."
+    "How about two more questions to earn some points? Can you guess how many National Parks I have visited? Hint: there are only 63 National Parks."
   );
 
   guesses--;
